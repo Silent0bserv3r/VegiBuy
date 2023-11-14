26e8ec587f85b06fdbe89ce0dea9e8510d64f771
Gaurav Chaubey <gauravchaubey807@gmail.com>
2023-11-14 20:08:37 +0530

    Setup Changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div id="nav-wrapper">
+                <div id="logo">
+                    VegiBuy
+                </div>
+                <div id="search-wrapper">
+                    Search
+                </div>
+                div#location-wrapper{Location}+div#
+            </div>
+        </nav>
+    );
+}
